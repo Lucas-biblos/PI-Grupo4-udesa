e0f08ab3c5dcca07bbab7e0b817413eca0c31df5
Lucas-biblos <lbiblos@udesa.edu.ar>
2023-11-08 13:01:33 -0300

    agregamos algunos detalles

--- js/busqueda.js
+++ js/busqueda.js
@@ -1,26 +1,24 @@
 let apiKey = "055556ae06143e409a5fa45eab6c68d1";
 
-url = "https://api.themoviedb.org/3/search/movie?query=Avengers"
+let queryString = location.search;
+let queryStringObj = new URLSearchParams(queryString);
+let resulBusqueda = queryStringObj.get("buscar");
 
-headers = {
-    "accept": "application/json",
-    "Authorization": "Bearer 055556ae06143e409a5fa45eab6c68d1"
-}
-
-response = requests.get(url, headers=headers);
-
+urlBusqueda = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${queryStringObj}`
 
 let urlPopulares = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`
 let urlValoradas = `https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}`
 let urlGeneros = `https://api.themoviedb.org/3/movie/list?api_key=${apiKey}`
 
 
-fetch(url)
+
+
+fetch(urlBusqueda)
 .then(function(response) {
     return response.json();
 })
 .then(function(data) {
-
+     console.log(data)
 })
 .catch(function(errors) {
     console.log(errors);
