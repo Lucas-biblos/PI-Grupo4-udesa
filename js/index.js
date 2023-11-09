5e6aa71c0fcbe19f81876bacff13701240a82448
GregoBonardi <gbonardi@udesa.edu.ar>
2023-11-09 11:38:09 -0300

    cambios

---
+++ js/index.js
@@ -0,0 +1,12 @@
+
+
+fetch()
+.then(function(response) {
+    return response.json();
+})
+.then(function(data) {
+     console.log(data)
+})
+.catch(function(errors) {
+    console.log(errors);
+});
