675f660ec5ecc99887e5382dd81d1d7d584c6bee
Lucas-biblos <lbiblos@udesa.edu.ar>
2023-11-10 17:07:28 -0300

    terminado index y empezado peliculas y busqueda

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ let urlPopularMovies= `https://api.themoviedb.org/3/movie/popular?api_key=${apiK
 let urlTopRatedSeries= `https://api.themoviedb.org/3/tv/top_rated?api_key=${apiKey}`;
 let urlPopularSeries= `https://api.themoviedb.org/3/tv/popular?api_key=${apiKey}`;
 
-let contenedorpelipop = document.querySelector(".contenedorpeliculaspopulares")
+let contenedorpelipop = document.querySelector("#contenedorpeliculaspopulares")
 
 fetch(urlTopRatedMovies)
 
@@ -20,7 +20,7 @@ fetch(urlTopRatedMovies)
      for (let i = 0; i < 5; i++) {
          
          console.log(miData[i]);
-         contenido += `  <li > <a href="./pelicula.html?id=${miData[i].id}"><img class="Fotos-seriepopular" src="./img/Legoinfinitywar.jpeg" alt="Avengers infinity war"></a>
+         contenido += `  <li > <a href="./pelicula.html?id=${miData[i].id}"><img class="Fotos-seriepopular" src="https://image.tmdb.org/t/p/w342/${miData[i].poster_path}"></a>
          <h2 class="h2deindex">${miData[i].title}</h2>
          <h3 class="h3deindex">${miData[i].release_date} </h3>
          </li> ` 
@@ -35,44 +35,56 @@ fetch(urlTopRatedMovies)
 });
 
 
-
+let peliculasP = document.querySelector("#peliculasP")
 
 fetch(urlPopularMovies)
 .then(function(response) {
     return response.json();
 })
 .then(function(data) {
-     console.log(data)
-})
+    console.log(data)
+     let miData = data.results;
+     let contenido = "";
+     for (let i = 0; i < 5; i++) {
+         
+         console.log(miData[i]);
+         contenido += `  <li > <a href="./pelicula.html?id=${miData[i].id}"><img class="Fotos-seriepopular" src="https://image.tmdb.org/t/p/w342/${miData[i].poster_path}"></a>
+         <h2 class="h2deindex">${miData[i].title}</h2>
+         <h3 class="h3deindex">${miData[i].release_date} </h3>
+         </li> ` 
+     }
+     
+     peliculasP.innerHTML = contenido;
+     
+     })
+
 .catch(function(errors) {
     console.log(errors);
 });
 
-
-
-
-fetch(urlTopRatedSeries)
-.then(function(response) {
-    return response.json();
-})
-.then(function(data) {
-     console.log(data)
-})
-.catch(function(errors) {
-    console.log(errors);
-});
-
-
-
-
+let seriesP = document.querySelector("#seriesP")
 
 fetch(urlPopularSeries)
 .then(function(response) {
     return response.json();
 })
 .then(function(data) {
-     console.log(data)
-})
+    console.log(data)
+    let miData = data.results;
+    let contenido = "";
+    for (let i = 0; i < 5; i++) {
+        
+        console.log(miData[i]);
+        contenido += `  <li > <a href="./Series.html?id=${miData[i].id}"><img class="Fotos-seriepopular" src="https://image.tmdb.org/t/p/w342/${miData[i].poster_path}"></a>
+        <h2 class="h2deindex">${miData[i].name}</h2>
+        <h3 class="h3deindex">${miData[i].first_air_date} </h3>
+        </li> ` 
+    }
+    
+    seriesP.innerHTML = contenido;
+    
+    })
+
 .catch(function(errors) {
-    console.log(errors);
+   console.log(errors);
 });
---
+++ js/series.js
@@ -0,0 +1,3 @@
+
+
+`https://api.themoviedb.org/3/tv/${id_serie}?api_key=${acaVaLaAPIKey}`
