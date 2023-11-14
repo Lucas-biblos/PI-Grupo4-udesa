cdf2304497e177eab2f26a1d5ef367dd204d9c24
Lucas-biblos <lbiblos@udesa.edu.ar>
2023-11-14 16:03:40 -0300

    avanzamos en detalle pelicula

--- js/pelicula.js
+++ js/pelicula.js
@@ -5,6 +5,7 @@ let qsobj = new URLSearchParams(qs);
 let idpelicula = qsobj.get("id")
 
 
+
 let url = `https://api.themoviedb.org/3/movie/${idpelicula}?api_key=${apiKey}`
 
 fetch(url)
@@ -17,27 +18,51 @@ fetch(url)
 
     console.log(data);
 
-     let miData = data.results;
-
-     let section = document.querySelector("#sectiondetalle");
-
-     let contenido = `<h1 class="h1depeliculas"><u></u></h1>
-        <p class="h1depeliculas"> <u>Calificacion:</u></p>
-        <p class="h1depeliculas"><u>Fecha de estreno:</u> 26 de abril de 2018</p>
-        <p class="h1depeliculas"><u>Duracion:</u> 2h 29m</p>
-        <p class="h1depeliculas"><u>Sinopsis:</u> El poderoso Thanos, decidido a hacerse con las Gemas del Infinito para controlar el universo, ataca la nave en la que huyen los supervivientes de Asgard. Su intención es hacerse con la segunda de las gemas, ahora en manos de Loki.</p>
-        <p class="h1depeliculas"><u>Genero:</u> <a class="celulargenero" href="./detalle-generos.html">Accion </a> y <a class="celulargenero" href="./detalle-generos.html">Ciencia Ficcion</a></p>
-        <p class="h1depeliculas"><u>Trailer:</u></p>
-        <iframe class="iframecelular" width="860" height="430" src="https://www.youtube.com/embed/6ZfuNTqbHE8?si=Zdw4qD3hCYP0oEoI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    let imagen = document.querySelector(".imgdepeliculas");
+    let titulo = document.querySelector(".h1depeliculas");
+    let rating = document.querySelector("#rating");
+    let fecha = document.querySelector("#fecha");
+    let duracion = document.querySelector("#duracion");
+    let sinopsis = document.querySelector("#sinopsis");
+    let trailer = document.querySelector("#trailer");
     
-        <article class="btn">
-            <button class="botonpeliculas"> ⭐Favoritos</button>
-    
-        </article>`;
-    
-     section.innerHTML = contenido
+    if (data.poster_path == null) {
+        imagen.src = "./img/noImage.png"
+    }
+    else {
+        imagen = `https://image.tmdb.org/t/p/w500${data.poster_path}`;
+    }
 
-     })
+    titulo.innerHTML += data.title;
+    rating.innerHTML += data.vote_average
+    fecha.innerHTML += data.release_date;
+    duracion.innerHTML += data.runtime;
+    sinopsis.innerHTML += data.overview;
+    
+    let genero = ""
+        let midata = data
+        let capturo = document.querySelector("#genero")
+
+        if (midata.genres == null || midata.genres == 0) {
+            genero += `<p class="h1depeliculas" id="genero"><u>Genero:</u> <a class="celulargenero" href="./detalle-generos.html">No hay generos para la pelicula</a></p>`
+        }
+
+
+        for (let i = 0; i < midata.genres.length; i++) {
+            genero +=
+                `<p class="h1depeliculas" id="genero"><u>Genero:</u> <a class="celulargenero" href="./detalle-generos.html">${genres[i].name} </a></p>`
+                
+        }
+        capturo.innerHTML += genero;
+
+
+    
+
+
+    
+    
+    
+        })
 
 .catch(function(errors) {
     console.log(errors);
